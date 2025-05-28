766f88c12e85761c7654e5f6434a250a909b6bfe
smitk <smitkachhad2000@gmail.com>
2025-05-28 19:15:18 +0530

    add

---
+++ src/Basic/Hello.java
@@ -0,0 +1,14 @@
+package Basic;
+
+public class Hello {
+    public static void main(String a[])
+    {
+        int num= 233;
+        float num2= 2.5F;
+        System.out.println("Hello world");
+        System.out.println(num);
+        System.out.println(num2);
+    }
+}
+
+
---
+++ src/Basic/TypeCast_2.java
@@ -0,0 +1,19 @@
+package Basic;
+
+public class TypeCast_2 {
+    public static void main(String[] args) {
+
+        int a=20;
+        float f=a;
+        System.out.println(f); //inplicit casting
+
+        float f1=30.4f;
+        int a1= (int) f1;
+        System.out.println(a1); //explicit casting
+
+        int i=258;
+        byte b= (byte) i;
+        System.out.println(b); //moduler  divide by byte size 257 and module will print
+
+    }
+}

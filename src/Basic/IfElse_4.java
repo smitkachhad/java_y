470ff0ae488531594cff59fbb643bdd6dc828482
smitk <smitkachhad2000@gmail.com>
2025-06-07 23:21:24 +0530

    add

---
+++ src/Basic/IfElse_4.java
@@ -0,0 +1,22 @@
+package Basic;
+
+public class IfElse_4 {
+    public static void main(String[] args) {
+
+        int x=91;
+        int y=922;
+        int z=93;
+
+        if (x>y && x>z)
+        {
+            System.out.println(x);
+        } else if (y>z) {
+            System.out.println(y);
+
+        }
+        else
+            System.out.println(z);
+
+
+    }
+}
